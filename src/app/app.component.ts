b39a439a4e4a59151238ca9b5a526b7aee7d5be6
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-23 12:26:53 +0100

    fix typo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   currentDate = new Date();
-  currentTemperaturs = {
+  currentTemperatures = {
     berlin: 4.2749812,
     newYork: 18.1214,
     paris: 72.1209001,
